3b646bd64690df586cfc5cc025d221f496f28cd4
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-04-17 14:09:43 +0300

    feat: update api gateway

--- api-gateway/app/Http/Controllers/UserController.php
+++ api-gateway/app/Http/Controllers/UserController.php
@@ -15,13 +15,23 @@ class UserController extends Controller
             'password' => 'required|string|min:8'
         ]);
 
+        if (User::where('email', $fields['email'])->exists()) {
+            return response([
+                'message' => 'User already exists'
+            ], 409);
+        }
+
         $user = User::create([
             'name' => $fields['name'],
             'email' => $fields['email'],
             'password' => bcrypt($fields['password']),
         ]);
 
-        $token = $user->createToken('api-token')->plainTextToken;
+        $token = $user->createToken(
+            'api-token',
+            ['*'],
+            now()->addWeek()
+        )->plainTextToken;
 
         $response = [
             'user' => $user,
@@ -33,7 +43,19 @@ class UserController extends Controller
 
     public function profile(Request $request)
     {
-        return $request->user();
+        $fields = $request->validate([
+            'email' => 'required|string|email|max:255',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user) {
+            return response([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        return response($user, 200);
     }
 
     public function update(Request $request)
--- api-gateway/routes/api.php
+++ api-gateway/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TokenController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/user/register', [UserController::class, 'register']);
+Route::put('/user/update', [UserController::class, 'update'])->middleware('auth:sanctum');
+Route::get('/user/profile', [UserController::class, 'profile'])->middleware('auth:sanctum');
+
+Route::post('/token/create', [TokenController::class, 'create']);
+Route::delete('/token/delete', [TokenController::class, 'revoke'])->middleware('auth:sanctum');
+Route::put('/token/refresh', [TokenController::class, 'refresh'])->middleware('auth:sanctum');
+
