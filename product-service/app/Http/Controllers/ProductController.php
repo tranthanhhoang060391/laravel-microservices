939aabc52751a3542627a924e967322acd530270
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-04-21 14:43:32 +0300

    feat: add Product controller for product service

---
+++ product-service/app/Http/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'status' => 'required|string|in:available,unavailable,discontinued',
+        ]);
+
+        $response = Product::create($fields);
+
+        return response()->json($response, 201);
+    }
+
+
+}
