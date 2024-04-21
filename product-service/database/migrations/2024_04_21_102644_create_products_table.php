293950a14fd00ed33fbf7df04264389b4287a6b8
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-04-21 13:30:06 +0300

    feat: add controller and migration for product service

---
+++ product-service/database/migrations/2024_04_21_102644_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            $table->string('status');
+            $table->string('photo');
+            $table->softDeletes();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamp('archived_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->index('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
