2e1d4de6614b78276a1ccd30d1c93c20746909d3
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-04-21 19:48:30 +0300

    feat: add tables for order service

--- order-service/database/migrations/2024_04_21_151303_create_order_details_table.php
+++ order-service/database/migrations/2024_04_21_151303_create_order_details_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->string('order_number')->unique();
-            $table->integer('user_id');
-            $table->string('status');
-            $table->float('total');
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->foreignId('order_id')->constrained('orders', 'id');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_details');
     }
 };
