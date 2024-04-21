51f89ab43a32ad459103556ef01b0c3317d10efa
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-04-21 20:37:23 +0300

    feat: update order service

--- order-service/app/Models/Order.php
+++ order-service/app/Models/Order.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_number', 'user_id', 'total', 'status'];
 }
--- order-service/app/Models/OrderDetail.php
+++ order-service/app/Models/OrderDetail.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id', 'quantity', 'order_id'];
 }
