44baedfa262e48d16fa88e92cc2bb55dd1358c7e
tranthanhhoang060391 <tranthanhhoang060391@gmail.com>
2024-05-05 20:45:30 +0300

    feat: add token table

--- order-service/routes/api.php
+++ order-service/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 
-Route::get('/orders', [OrderController::class, 'index']);
-Route::post('/order/create', [OrderController::class, 'create']);
-Route::get('/order/{id}', [OrderController::class, 'show']);
-Route::put('/order/update/{id}', [OrderController::class, 'update']);
-Route::delete('/order/delete/{id}', [OrderController::class, 'delete']);
-Route::get('/order/user/{user_id}', [OrderController::class, 'userOrders']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::post('/order/create', [OrderController::class, 'create']);
+    Route::get('/order/{id}', [OrderController::class, 'show']);
+    Route::put('/order/update/{id}', [OrderController::class, 'update']);
+    Route::delete('/order/delete/{id}', [OrderController::class, 'delete']);
+    Route::get('/order/user/{user_id}', [OrderController::class, 'userOrders']);
+});
--- product-service/routes/api.php
+++ product-service/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/product/create', [ProductController::class, 'create']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::put('/product/update/{id}', [ProductController::class, 'update']);
-Route::delete('/product/delete/{id}', [ProductController::class, 'delete']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/product/create', [ProductController::class, 'create']);
+    Route::get('/product/{id}', [ProductController::class, 'show']);
+    Route::put('/product/update/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/delete/{id}', [ProductController::class, 'delete']);
+});
